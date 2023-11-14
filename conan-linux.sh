b611b2b702685fd39baa430dcfcd7874f40deac0
Igor Popov <popov.nirvana@gmail.com>
2023-11-14 15:24:56 +0300

    Linux script

--- conan-linux.sh
+++ conan-linux.sh
@@ -1,5 +1,5 @@
 for prof in profiles/linux-*.profile
 do
-	conan create . -pr:h "$prof" -pr:b "$prof" --build=missing -s build_type=Debug
-	conan create . -pr:h "$prof" -pr:b "$prof" --build=missing -s build_type=Release
+	conan create . -pr:h "$prof" -pr:b "$prof" --build=missing -s:h build_type=Debug -s:b build_type=Debug
+	conan create . -pr:h "$prof" -pr:b "$prof" --build=missing -s:h build_type=Release -s:b build_type=Release
 done
